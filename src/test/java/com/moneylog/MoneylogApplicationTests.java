f14fabcef85cd9c98b3bbda39e95d385aaac8b01
piopoi <kangsw1988@gmail.com>
2023-11-10 00:36:26 +0900

    setup: add project

---
+++ src/test/java/com/moneylog/MoneylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneylogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
