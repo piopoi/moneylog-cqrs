00067a3e345a391aa7dd28ea1183cb64765a58f9
piopoi <kangsw1988@gmail.com>
2023-11-19 23:45:59 +0900

    feat #17: 지출 읽기(목록) API 구현
    - 필터링: `지출일시`, `지출액`, `카테고리`
    - 조회된 모든 지출의 `지출 합계`, `카테고리 별 지출 합계`를 같이 반환.

--- src/main/java/com/moneylog/api/expense/dto/ExpenseGetResponse.java
+++ src/main/java/com/moneylog/api/expense/dto/ExpenseGetResponse.java
@@ -1,6 +1,6 @@
 package com.moneylog.api.expense.dto;
 
-import com.moneylog.api.category.domain.Category;
+import com.moneylog.api.category.dto.CategoryGetResponse;
 import com.moneylog.api.expense.domain.Expense;
 import java.time.LocalDateTime;
 import lombok.Builder;
@@ -13,30 +13,16 @@ public class ExpenseGetResponse {
     private final Long expenseId;
     private final LocalDateTime expendedAt;
     private final Long expenseAmount;
-    private final CategoryResponse category;
+    private final CategoryGetResponse category;
     private final String memo;
     private final Boolean isExcludeTotal;
-
-    @Getter
-    @Builder
-    public static class CategoryResponse {
-        private final Long categoryId;
-        private final String categoryName;
-
-        public static CategoryResponse from(Category category) {
-            return CategoryResponse.builder()
-                    .categoryId(category.getId())
-                    .categoryName(category.getName())
-                    .build();
-        }
-    }
 
     public static ExpenseGetResponse from(Expense expense) {
         return ExpenseGetResponse.builder()
                 .expenseId(expense.getId())
                 .expendedAt(expense.getExpendedAt())
                 .expenseAmount(expense.getExpenseAmount())
-                .category(CategoryResponse.from(expense.getCategory()))
+                .category(CategoryGetResponse.from(expense.getCategory()))
                 .memo(expense.getMemo())
                 .isExcludeTotal(expense.getIsExcludeTotal())
                 .build();
